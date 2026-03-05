283d1c806648035121800cea1b2613ae5df84dfb
JulezD3v <jul3zdev@gmail.com>
2026-03-05 18:36:51 +0300

    mainscreen works

--- lib/Controller/control_circle.dart
+++ lib/Controller/control_circle.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import '/Core/colors/colors.dart';
-import 'mycontroller.dart';
 import 'blecontroller_widget.dart';
 
 class DirectionalControl extends StatelessWidget {
@@ -14,10 +12,6 @@ class DirectionalControl extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // Assuming these are defined in your AppColors
-    // final bgColor = AppColors.bgColor;           // e.g. Color(0xFFE0E0E0) or off-white
-    // final primaryGreen = AppColors.primaryGreen; // e.g. Colors.teal[700]
-
     return SizedBox(
       width: 250,
       height: 250,
@@ -32,7 +26,7 @@ class DirectionalControl extends StatelessWidget {
               shape: BoxShape.circle,
               color: Colors.transparent,
               border: Border.all(
-                color: Colors.blueGrey.withOpacity(0.15), // very light blue-grey
+                color: Colors.blueGrey.withOpacity(0.15),
                 width: 14,
               ),
             ),
@@ -40,11 +34,11 @@ class DirectionalControl extends StatelessWidget {
 
           // 2. Main neumorphic circle
           Container(
-            width: 220, // slightly smaller than outer ring
+            width: 220,
             height: 220,
             decoration: BoxDecoration(
               shape: BoxShape.circle,
-              color: AppColors.bgColor, // your background color
+              color: AppColors.bgColor,
               boxShadow: [
                 BoxShadow(
                   color: Colors.white.withOpacity(0.9),
@@ -63,25 +57,25 @@ class DirectionalControl extends StatelessWidget {
           ),
 
           // 3. Four arrow buttons around the edge
-         Positioned(
-  top: 20,
-  child: _buildArrowButton(context, Icons.arrow_upward_rounded),
-),
-Positioned(
-  bottom: 20,
-  child: _buildArrowButton(context, Icons.arrow_downward_rounded),
-),
-Positioned(
-  left: 20,
-  child: _buildArrowButton(context, Icons.arrow_back_rounded),
-),
-Positioned(
-  right: 20,
-  child: _buildArrowButton(context, Icons.arrow_forward_rounded),
-),
+          Positioned(
+            top: 20,
+            child: _buildArrowButton(Icons.arrow_upward_rounded),
+          ),
+          Positioned(
+            bottom: 20,
+            child: _buildArrowButton(Icons.arrow_downward_rounded),
+          ),
+          Positioned(
+            left: 20,
+            child: _buildArrowButton(Icons.arrow_back_rounded),
+          ),
+          Positioned(
+            right: 20,
+            child: _buildArrowButton(Icons.arrow_forward_rounded),
+          ),
           // 4. Center icon (not a button)
-          const Icon(
-            Icons.open_with_rounded, // or Icons.drag_indicator, etc.
+          Icon(
+            Icons.open_with_rounded,
             size: 48,
             color: AppColors.primaryGreen,
           ),
@@ -90,46 +84,43 @@ Positioned(
     );
   }
 
- Widget _buildArrowButton(BuildContext context, IconData icon) {
-  final controller = Provider.of<MyBleController>(context, listen: false);
-  //final ble = Provider.of<MyBleController>(context, listen: false);
-
-  return GestureDetector(
-    onTap: () {
-  if (controller.isConnected) {
-    String cmd = '';
-    if (icon == Icons.arrow_upward_rounded)    cmd = 'F';
-    if (icon == Icons.arrow_downward_rounded)  cmd = 'B';
-    if (icon == Icons.arrow_back_rounded)      cmd = 'L';
-    if (icon == Icons.arrow_forward_rounded)   cmd = 'R';
-    controller.sendCommand(cmd);
+  Widget _buildArrowButton(IconData icon) {
+    return GestureDetector(
+      onTap: () {
+        if (controller.isConnected) {
+          String cmd = '';
+          if (icon == Icons.arrow_upward_rounded)    cmd = 'F';
+          if (icon == Icons.arrow_downward_rounded)  cmd = 'B';
+          if (icon == Icons.arrow_back_rounded)      cmd = 'L';
+          if (icon == Icons.arrow_forward_rounded)   cmd = 'R';
+          controller.sendCommand(cmd);
+        }
+      },
+      child: Container(
+        width: 56,
+        height: 56,
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          color: AppColors.bgColor,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.white.withOpacity(0.7),
+              offset: const Offset(-4, -4),
+              blurRadius: 8,
+            ),
+            BoxShadow(
+              color: Colors.black.withOpacity(0.15),
+              offset: const Offset(4, 4),
+              blurRadius: 8,
+            ),
+          ],
+        ),
+        child: Icon(
+          icon,
+          size: 32,
+          color: AppColors.primaryGreen.withOpacity(0.9),
+        ),
+      ),
+    );
   }
-},
-    child: Container(
-      width: 56,
-      height: 56,
-      decoration: BoxDecoration(
-        shape: BoxShape.circle,
-        color: AppColors.bgColor,
-        boxShadow: [
-          BoxShadow(
-            color: Colors.white.withOpacity(0.7),
-            offset: const Offset(-4, -4),
-            blurRadius: 8,
-          ),
-          BoxShadow(
-            color: Colors.black.withOpacity(0.15),
-            offset: const Offset(4, 4),
-            blurRadius: 8,
-          ),
-        ],
-      ),
-      child: Icon(
-        icon,
-        size: 32,
-        color: AppColors.primaryGreen.withOpacity(0.9),
-      ),
-    ),
-  );
-}
 }
--- lib/Controller/mycontroller.dart
+++ lib/Controller/mycontroller.dart
@@ -18,19 +18,18 @@ class _ControllerState extends State<Controller> {
   bool stopActive = false;
   bool startHighlight = false;
   bool stopHighlight = false;
-bool isConnected = false;
-final MyBleController _controller = MyBleController();
-@override
-void initState() {
-  super.initState();
-}
+  bool isConnected = false;
+  final MyBleController _controller = MyBleController();
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   void dispose() {
     _controller.dispose();
     super.dispose();
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -39,166 +38,169 @@ void initState() {
       body: SafeArea(
         child: ListenableBuilder(
           listenable: _controller,
-          builder: (context, child){
-           return SingleChildScrollView(
-            child: Column(
-              children: [
-                // Top Bar
-                Padding(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 20,
-                    vertical: 10,
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      const Icon(Icons.arrow_back_ios_new),
-                      const Text(
-                        "KyU Robotics Team",
-                        style: TextStyle(
-                          fontSize: 22,
-                          fontWeight: FontWeight.w600,
-                        ),
-                      ),
-                      Image.asset(
-                  "assets/schoolLogo.png",
-                  height: 40, // small
-                  fit: BoxFit.contain,
-                ),
-                    ],
-                  ),
-                ),
-          
-                const SizedBox(height: 16),
-          
-                // Connected Card
-               SoftBoxButton(
-                margin: const EdgeInsets.symmetric(horizontal: 50),
-                padding: const EdgeInsets.symmetric(vertical: 14),
-                onPressed: () {
-                  if (!_controller.isConnected) {
-                    _controller.connect();
-                  }
-                },
-                child: Column(
-                  children: [
-                    Text(
-                      _controller.isConnected
-                          ? "BLUETOOTH CONNECTED"
-                          : "CONNECT TO ROBOT",
-                      style: TextStyle(
-                        color: _controller.isConnected
-                            ? AppColors.primaryGreen
-                            : Colors.grey,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 16,
-                        letterSpacing: 2,
-                      ),
+          builder: (context, child) {
+            return SingleChildScrollView(
+              child: Column(
+                children: [
+                  // Top Bar
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 20,
+                      vertical: 10,
                     ),
-                    SizedBox(height: 3),
-                    Text(
-                      _controller.isConnected
-                          ? _controller.status
-                          : "Tap to connect",
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Icon(Icons.arrow_back_ios_new),
+                        const Text(
+                          "KyU Robotics Team",
+                          style: TextStyle(
+                            fontSize: 22,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                        Image.asset(
+                          "assets/schoolLogo.png",
+                          height: 40, // small
+                          fit: BoxFit.contain,
+                        ),
+                      ],
                     ),
-                  ],
-                ),),
-                const SizedBox(height: 35),
-          
-                // Controller
-               DirectionalControl(controller: _controller),
-               
-                const SizedBox(height: 40),
-          
-                // Stop and switch to camera
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 20),
-                  child: Row(
-                    children: [
-                      Expanded(
-                        child: ToggleCard(
-                          title: "Stop",
-                          icon: Icons.stop_screen_share_sharp,
-                          active: stopActive,
-                          onTap: () {
-  setState(() {
-    stopActive = !stopActive;
-
-    // If Stop is pressed, Camera should remain ON
-    if (stopActive) {
-      startActive = true;
-    }
-  });
-},
-                        ),
-                      ),
-          
-                      const SizedBox(width: 20),
-          
-                      Expanded(
-                        child: ToggleCard(
-                          title: "Camera",
-                          icon: Icons.camera,
-                          active: startActive,
-                          onTap: () {
-  setState(() {
-    startActive = !startActive;
-
-    // If camera is turned off, stop should also turn off
-    if (!startActive) {
-      stopActive = false;
-    }
-  });
-},
-                        ),
-                      ),
-                    ],
                   ),
-                ),
-          
-                const SizedBox(height: 30),
-          
-                // Reduce & Boost Row
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 20),
-                  child:Row(
-            children: [
-             // Increase
-          Expanded(
-            child: ActionCard(
-              title: "Increase",
-              icon: Icons.electric_bolt_rounded,
-              highlight: _controller.isConnected,
-              onTap: _controller.isConnected
-          ? () => _controller.increaseSpeed()
-          : () {},
-            ),
-          ),
-          
-          // Decrease
-          Expanded(
-            child: ActionCard(
-              title: "Decrease ",
-              icon: Icons.slow_motion_video_rounded,
-              highlight: _controller.isConnected,
-              onTap: _controller.isConnected
-          ? () => _controller.decreaseSpeed()
-          : () {},
-            ),
-          ),
-            ],
-          )
-          ),
-                const SizedBox(height: 20),
-                Image.asset(
-                  "assets/schoolLogo.png",
-                  height: 40, // small
-                  fit: BoxFit.contain,
-                ),
-              ],
-            ),
-          );
-  }),
-  ), );
+
+                  const SizedBox(height: 16),
+
+                  // Connected Card
+                  SoftBoxButton(
+                    margin: const EdgeInsets.symmetric(horizontal: 50),
+                    padding: const EdgeInsets.symmetric(vertical: 14),
+                    onPressed: () {
+                      if (!_controller.isConnected) {
+                        _controller.connect();
+                      }
+                    },
+                    child: Column(
+                      children: [
+                        Text(
+                          _controller.isConnected
+                              ? "BLUETOOTH CONNECTED"
+                              : "CONNECT TO ROBOT",
+                          style: TextStyle(
+                            color: _controller.isConnected
+                                ? AppColors.primaryGreen
+                                : Colors.grey,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                            letterSpacing: 2,
+                          ),
+                        ),
+                        SizedBox(height: 3),
+                        Text(
+                          _controller.isConnected
+                              ? _controller.status
+                              : "Tap to connect",
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 35),
+
+                  // Controller
+                  DirectionalControl(controller: _controller),
+
+                  const SizedBox(height: 40),
+
+                  // Stop and switch to camera
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: ToggleCard(
+                            title: "Stop",
+                            icon: Icons.stop_screen_share_sharp,
+                            active: stopActive,
+                            onTap: () {
+                              setState(() {
+                                stopActive = !stopActive;
+
+                                // If Stop is pressed, Camera should remain ON
+                                if (stopActive) {
+                                  startActive = true;
+                                }
+                              });
+                            },
+                          ),
+                        ),
+
+                        const SizedBox(width: 20),
+
+                        Expanded(
+                          child: ToggleCard(
+                            title: "Camera",
+                            icon: Icons.camera,
+                            active: startActive,
+                            onTap: () {
+                              setState(() {
+                                startActive = !startActive;
+
+                                // If camera is turned off, stop should also turn off
+                                if (!startActive) {
+                                  stopActive = false;
+                                }
+                              });
+                            },
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+
+                  const SizedBox(height: 30),
+
+                  // Reduce & Boost Row
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    child: Row(
+                      children: [
+                        // Increase
+                        Expanded(
+                          child: ActionCard(
+                            title: "Increase",
+                            icon: Icons.electric_bolt_rounded,
+                            highlight: _controller.isConnected,
+                            onTap: _controller.isConnected
+                                ? () => _controller.increaseSpeed()
+                                : () {},
+                          ),
+                        ),
+
+                        // Decrease
+                        Expanded(
+                          child: ActionCard(
+                            title: "Decrease ",
+                            icon: Icons.slow_motion_video_rounded,
+                            highlight: _controller.isConnected,
+                            onTap: _controller.isConnected
+                                ? () => _controller.decreaseSpeed()
+                                : () {},
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  Image.asset(
+                    "assets/schoolLogo.png",
+                    height: 40, // small
+                    fit: BoxFit.contain,
+                  ),
+                ],
+              ),
+            );
+          },
+        ),
+      ),
+    );
   }
 }
